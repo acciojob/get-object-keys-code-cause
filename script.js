7980ca7e37eb9039740655ddd05248648a5f46b8
Ashish Pandey <writeashish22@gmail.com>
2025-02-16 15:43:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
-//your JS code here. If required.
-const student ={
-	name: "Ashish",
-	age: 18
+// //your JS code here. If required.
+// const student ={
+// 	name: "Ashish",
+// 	age: 18
+// }
+
+// function getKeys(obj){
+// 	return Object.keys(obj);
+// }
+
+// console.log(getKeys(student));
+
+
+
+
+
+
+
+// Define function first
+function getKeys(obj) {
+  return Object.keys(obj);
 }
 
-function getKeys(obj){
-	return Object.keys(obj);
-}
+// Create student object with only "name" property
+const student = {
+  name: "John"
+};
 
-console.log(getKeys(student));
+// Test the function
+console.log(getKeys(student)); // Expected output: ["name"]
