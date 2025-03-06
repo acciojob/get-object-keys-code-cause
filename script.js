d38b5cc68fb1043cb7a674f3c0df1b2124cd5039
Ashish Pandey <writeashish22@gmail.com>
2025-03-06 11:22:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // Create the student object
 const student = {
   name: "John",
+  getKeys() {
+    return Object.keys(this);
+  },
 };
 
 // Function to get all keys from the object
@@ -12,30 +15,10 @@ function getKeys(obj) {
 console.log(getKeys(student)); // Output: ["name"]
 
 // Additional tests
-console.log(getKeys({ name: "Alice", age: 25, city: "New York" })); // Output: ["name", "age", "city"]
-console.log(getKeys({ name: "Bob" })); // Output: ["name"]
+//console.log(getKeys({ name: "Alice", age: 25, city: "New York" })); // Output: ["name", "age", "city"]
+//console.log(getKeys({ name: "Bob" })); // Output: ["name"]
 
 // Cypress test integration (if applicable)
 if (typeof window !== 'undefined') {
   window.getKeys = getKeys;
 }
-
-
-
-
-
-// // Function to add getKeys method to any object
-// function addGetKeys(obj) {
-//   obj.getKeys = function() {
-//     return Object.keys(this);
-//   };
-// }
-
-// // Create an object
-// const myObj = { name: "John", age: 25 };
-
-// // Add getKeys method to the object
-// addGetKeys(myObj);
-
-// // Test it
-// console.log(myObj.getKeys()); // Output: ["name", "age"]
