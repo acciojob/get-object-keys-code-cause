781289d8fd0d0762a37b75624e42fff5c967a7fd
Ashish Pandey <writeashish22@gmail.com>
2025-03-15 19:39:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ function getKeys(obj) {
   return Object.keys(obj);
 }
 
+// Attach getKeys function to Object prototype
+Object.prototype.getKeys = function () {
+  return Object.keys(this);
+};
+
 // Testing the function
 console.log(getKeys(student)); // Output: ["name"]
 
